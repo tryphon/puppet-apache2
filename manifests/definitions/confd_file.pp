970c9e099fad57b66d591e50809e0fabffd1b745
Alban Peignier <alban.peignier@free.fr>
2010-10-27 12:42:09 +0200

    Support conf.d/$name.$fqdn in apache2::confd_file

--- manifests/definitions/confd_file.pp
+++ manifests/definitions/confd_file.pp
@@ -1,6 +1,6 @@
 define apache2::confd_file($source = '') {
   $real_source = $source ? {
-    '' => ["puppet:///files/apache2/conf.d/$name", "puppet:///apache2/conf.d/$name"],
+    '' => ["puppet:///files/apache2/conf.d/$name.$fqdn", "puppet:///files/apache2/conf.d/$name", "puppet:///apache2/conf.d/$name"],
     default => $source
   }
     
