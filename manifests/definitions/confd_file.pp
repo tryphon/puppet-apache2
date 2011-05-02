23c4c7bd86a03b7ae27dd2a77a42edbefdf90512
Alban Peignier <alban@tryphon.eu>
2011-05-02 11:17:49 +0200

    Support content in apache2::confd_file

--- manifests/definitions/confd_file.pp
+++ manifests/definitions/confd_file.pp
@@ -1,12 +1,20 @@
-define apache2::confd_file($source = '') {
-  $real_source = $source ? {
-    '' => ["puppet:///files/apache2/conf.d/$name.$fqdn", "puppet:///files/apache2/conf.d/$name", "puppet:///apache2/conf.d/$name"],
-    default => $source
-  }
-    
-  file { "/etc/apache2/conf.d/$name":
-    source => $real_source,
-    notify => Service[apache2],
-    require => Package[apache2]
+define apache2::confd_file($source = '', $content = false) {
+  if $content {
+    file { "/etc/apache2/conf.d/$name":
+      content => $content,
+      notify => Service[apache2],
+      require => Package[apache2]
+    }
+  } else {
+    $real_source = $source ? {
+      '' => ["puppet:///files/apache2/conf.d/$name.$fqdn", "puppet:///files/apache2/conf.d/$name", "puppet:///apache2/conf.d/$name"],
+      default => $source
+    }
+  
+    file { "/etc/apache2/conf.d/$name":
+      source => $real_source,
+      notify => Service[apache2],
+      require => Package[apache2]
+    }
   }
 }
