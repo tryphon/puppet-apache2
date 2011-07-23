3e9bf37cf004040d9dad05cad32e2ee14324f0d9
Alban Peignier <alban@tryphon.eu>
2011-07-23 11:14:09 +0200

    Support both lenny and squeeze

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -4,13 +4,17 @@ class apache2::passenger {
   include ruby::gems
 
   package { libapache2-mod-passenger: 
-    require => [Package[apache2], Apt::Sources_list[lenny-backports], Apt::Preferences[libapache2-mod-passenger]]
+    require => Package[apache2]
   }
 
-  apt::preferences { libapache2-mod-passenger:
-    package => libapache2-mod-passenger, 
-    pin => "release a=lenny-backports",
-    priority => 999
+  if $debian::lenny {
+    apt::preferences { libapache2-mod-passenger:
+      package => libapache2-mod-passenger, 
+      pin => "release a=lenny-backports",
+      priority => 999,
+      require => Apt::Sources_list[lenny-backports],
+      before => Package[libapache2-mod-passenger]
+    }
   }
 
   ruby::gem { fastthread: }
