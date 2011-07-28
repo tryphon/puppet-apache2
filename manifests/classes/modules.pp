b6ff484853a6cb55f8f654b2a05fa565e5270608
Alban Peignier <alban@tryphon.eu>
2011-07-28 17:17:27 +0200

    Fixes libapache2-mod-xsendfile apt preferencs for lenny

--- manifests/classes/modules.pp
+++ manifests/classes/modules.pp
@@ -32,13 +32,16 @@ class apache2::expires {
 
 class apache2::xsendfile {
   package { libapache2-mod-xsendfile: 
-    require => [Package[apache2], Apt::Preferences[libapache2-mod-passenger]]
+    require => Package[apache2]
   }
 
-  apt::preferences { libapache2-mod-xsendfile:
-    package => libapache2-mod-xsendfile, 
-    pin => "release a=lenny-backports",
-    priority => 999
+  if $debian::lenny {
+    apt::preferences { libapache2-mod-xsendfile:
+      package => libapache2-mod-xsendfile, 
+      pin => "release a=lenny-backports",
+      priority => 999,
+      before => Package[libapache2-mod-xsendfile]
+    }
   }
 
   apache2::module { xsendfile: }
