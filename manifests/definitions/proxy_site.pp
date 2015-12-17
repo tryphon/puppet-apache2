57d5f5e4b487f00ff8c91920f9414c7832dbd5b4
Alban Peignier <alban@tryphon.eu>
2015-12-17 08:37:27 +0100

    Add support for authentication for proxy_site

--- manifests/definitions/proxy_site.pp
+++ manifests/definitions/proxy_site.pp
@@ -1,8 +1,12 @@
-define apache2::proxy_site($target, $preserve_host = true, $content = '', $ensure = 'present') {
+define apache2::proxy_site($target, $preserve_host = true, $content = '', $ensure = 'present', $authentication = false) {
   include apache2::proxy::http
-  
+
   site { $name:
     content => template("apache2/reverse-proxy.conf"),
-    ensure => $ensure
+    ensure => $ensure,
+  }
+
+  if $authentication {
+    File['/etc/apache2/htpasswd'] -> Site[$name]
   }
 }
