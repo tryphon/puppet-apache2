7c9cd99d8c5bfb32a9ea1ff2fd3a939888f5f759
Florent Peyraud <florent@tryphon.eu>
2016-03-24 16:27:19 +0100

    branch for 24 continues

--- manifests/definitions/site.pp
+++ manifests/definitions/site.pp
@@ -47,3 +47,53 @@ define apache2::site($link = '', $source = '', $content = false, $ensure = 'pres
     }
   }
 }
+
+define apache24::site($link = '', $source = '', $content = false, $ensure = 'present') {
+  if $ensure == 'present' {
+    $real_source = $source ? {
+      '' => "puppet:///files/apache2/sites-available/$name.conf",
+      default => $source
+    }
+
+    # install and enable a given site configuration
+    if $content {
+      file { "/etc/apache2/sites-available/$name.conf":
+        content => $content,
+        notify => Service[apache2],
+        require => Package[apache2]
+      }
+    } else {
+      file { "/etc/apache2/sites-available/$name.conf":
+        source => $real_source,
+        notify => Service[apache2],
+        require => Package[apache2],
+        links => follow
+      }
+    }
+
+    file {
+      $link ? {
+        '' => "/etc/apache2/sites-enabled/$name.conf",
+        default => "/etc/apache2/sites-enabled/$link"
+      } :
+      ensure => "/etc/apache2/sites-available/$name.conf",
+      require => File["/etc/apache2/sites-available/$name.conf"],
+      notify => Service[apache2]
+    }
+  } else {
+    file { "/etc/apache2/sites-available/$name":
+      ensure => $ensure,
+      notify => Service[apache2],
+      require => Package[apache2]
+    }
+    file {
+      $link ? {
+        '' => "/etc/apache2/sites-enabled/$name",
+        default => "/etc/apache2/sites-enabled/$link"
+      } :
+      ensure => $ensure,
+      notify => Service[apache2],
+      require => Package[apache2]
+    }
+  }
+}
