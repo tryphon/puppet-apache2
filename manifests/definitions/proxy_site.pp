2d7a9e070535a2ef9bf89a0ac59673cfa0c6bfea
Florent Peyraud <florent@tryphon.eu>
2016-03-24 15:43:51 +0100

    reverse proxy configuration, the 2.4 way

--- manifests/definitions/proxy_site.pp
+++ manifests/definitions/proxy_site.pp
@@ -10,3 +10,15 @@ define apache2::proxy_site($target, $preserve_host = true, $content = '', $ensur
     File['/etc/apache2/htpasswd'] -> Site[$name]
   }
 }
+define apache24::proxy_site($target, $preserve_host = true, $content = '', $ensure = 'present', $authentication = false, $ssl = false, $aliases = []) {
+  include apache2::proxy::http
+
+  site { $name:
+    content => template("apache2/reverse-proxy-24.conf"),
+    ensure => $ensure,
+  }
+
+  if $authentication {
+    File['/etc/apache2/htpasswd'] -> Site[$name]
+  }
+}
