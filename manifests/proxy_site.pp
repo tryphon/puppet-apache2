7c1dfc48cfdd499e268f2961e7d85c26657090f3
Florent Peyraud <florent.peyraud@af83.com>
2018-04-26 21:40:38 +0200

    refacto

--- manifests/proxy_site.pp
+++ manifests/proxy_site.pp
@@ -1,5 +1,5 @@
 define apache2::proxy_site($target, $preserve_host = true, $content = '', $ensure = 'present', $authentication = false, $ssl = false, $force_ssl = false, $aliases = [], $proxy_params = "") {
-  include apache2::proxy::http
+  include apache2::proxy_http
 
   site { $name:
     content => template("apache2/reverse-proxy.conf"),
