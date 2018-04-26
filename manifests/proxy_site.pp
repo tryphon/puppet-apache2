b91b5c153c696760780fefe70b5a9311de1fc37c
Florent Peyraud <florent.peyraud@af83.com>
2018-04-26 21:12:57 +0200

    Allow adding parameters to reverse ProxyPass configuration

---
+++ manifests/proxy_site.pp
@@ -0,0 +1,12 @@
+define apache2::proxy_site($target, $preserve_host = true, $content = '', $ensure = 'present', $authentication = false, $ssl = false, $force_ssl = false, $aliases = [], $proxy_params = "") {
+  include apache2::proxy::http
+
+  site { $name:
+    content => template("apache2/reverse-proxy.conf"),
+    ensure  => $ensure,
+  }
+
+  if $authentication {
+    File['/etc/apache2/htpasswd'] -> Site[$name]
+  }
+}
